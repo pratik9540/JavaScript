ee65db71f495157fce5f378974c6f089f3fd28dd
pratik9540 <72371972+pratik9540@users.noreply.github.com>
2023-02-25 08:08:06 +0530

    Add files via upload

---
+++ practice-4.js
@@ -0,0 +1,13 @@
+let a = "pratik"
+
+console.log(a.toUpperCase())
+
+
+let str= "hello i am xyz"
+let amount =(str.slice(8))
+console.log(amount)
+
+let sentence = "hello i am wolf i live in forest"
+let word = "wolf1"
+
+console.log(sentence.includes(word))
